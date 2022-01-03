b535843199e0b9f9a9afb791f59f7382067f0b1e
fredy <fredy>
2022-01-03 17:02:33 -0500

    base menu full admin usuario registro

--- src/components/loging.js
+++ src/components/loging.js
@@ -1,146 +1,131 @@
-
 import Axios from "axios";
 import React, { useState } from "react";
 import Swal from "sweetalert2";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 function Loging() {
-    const [correo, setCorreo] = useState("");
-    const [contrasena, setContrasena] = useState("");
-    const [admin, setAdmin] = useState(Boolean);
-  
-    const Login = async (e) => {
-      e.preventDefault();
-  
-      /* este recivira los datos desda la pagina para ser comparados con los del backend */
-      const usuario = {
-        correo,
-        contrasena,
-      };
-      const respuesta = await Axios.post(
-        "http://localhost:4000/ingresar",
-        usuario
-      ); /* 
-          enviara los datos json de la constante usuario al backend */
-  
-      const mensaje =
-        respuesta.data
-          .mensage; /* extraemos el mensaje desde el backen creado para mostrarlo en el sweetalert2 */
-  
-      if (mensaje === "Ingresate login valido desde el backend") {
-       
-        const user2 = respuesta.data.user2;
-        const admin = respuesta.data.admin;
-        const token = respuesta.data.token;
-        const nombre =
-          respuesta.data
-            .nombre; /* despuesde recibir el nombre para mostrarl oen el navvar */
-        const id = respuesta.data.id;
-         
-        /* const nombre=respuesta.data.nombre */
-        sessionStorage.setItem("token", token);
-        sessionStorage.setItem(
-          "nombre",
-          nombre
-        ); /* llevo el nombre al secion estorange que se hace publica en cualquier momento para ser usada */
-        sessionStorage.setItem("id", id);
-        sessionStorage.setItem("admin", admin);
-        /* sessionStorage.setItem('nombre', nombre) */
-        sessionStorage.setItem("user2", user2);
-       
-        Swal.fire({
-          icon: "success",
-          title: mensaje,
-          showConfirmButton: false,
-        });
-        /* lo direcciona a la pagina seleccionada despues de tener el login correcto */
-        setTimeout(() => {
-          if (admin === nombre) {
-            window.location.href = "/visualAdmin";
-          } /*vista para el admin */ else {
-            window.location.href = '/VistaProductosUsuario';
-           /*  window.location.href = '/VistaProductosUsuario/personal'; */
-          } /*vista para el ususario */
-        }, 1500);
-      } else {
-        Swal.fire({
-          icon: "error",
-          title: mensaje,
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      }
+  const [correo, setCorreo] = useState("");
+  const [contrasena, setContrasena] = useState("");
+  const [admin, setAdmin] = useState(Boolean);
+
+  const Login = async (e) => {
+    e.preventDefault();
+
+    /* este recivira los datos desda la pagina para ser comparados con los del backend */
+    const usuario = {
+      correo,
+      contrasena,
     };
-    return (
-      <div className="row ">
-        <div className="container p-5 mt-4">
-          
+    const respuesta = await Axios.post(
+      "http://localhost:4000/ingresar",
+      usuario
+    ); /* 
+          enviara los datos json de la constante usuario al backend */
+
+    const mensaje =
+      respuesta.data
+        .mensage; /* extraemos el mensaje desde el backen creado para mostrarlo en el sweetalert2 */
+
+    if (mensaje === "Ingresate login valido desde el backend") {
+      const user2 = respuesta.data.user2;
+      const admin = respuesta.data.admin;
+      const token = respuesta.data.token;
+      const nombre = respuesta.data.nombre;
+      /* despuesde recibir el nombre para mostrarl oen el navvar */
+      const id = respuesta.data.id;
+
+      /* const nombre=respuesta.data.nombre */
+      sessionStorage.setItem("token", token);
+      sessionStorage.setItem("nombre",nombre); /* llevo el nombre al secion estorange que se hace publica en cualquier momento para ser usada */
+      sessionStorage.setItem("id", id);
+      sessionStorage.setItem("admin", admin);
+      /* sessionStorage.setItem('nombre', nombre) */
+      sessionStorage.setItem("user2", user2);
+
+      Swal.fire({
+        icon: "success",
+        title: mensaje,
+        showConfirmButton: false,
+      });
+      /* lo direcciona a la pagina seleccionada despues de tener el login correcto */
+      setTimeout(() => {
+        if (admin === nombre) {
+          window.location.href = "/visualAdmin";
+        } /*vista para el admin */ else {
+          window.location.href = "/VistaProductosUsuario";
+          /*  window.location.href = '/VistaProductosUsuario/personal'; */
+        } /*vista para el ususario */
+      }, 1500);
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: mensaje,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+  return (
+    <div className="row ">
+      <div className="container p-5 mt-4"></div>
+      <div className="col-lg-6 col-md-8 mx-auto">
+        <div className="card">
+          <div className="container text-center fa-4x">
+            <i class="fas fa-user-lock"></i>
+          </div>
+          <div className="card-header text-center">Iniciar Sesion</div>
+
+          <div className="card-boddy m-2">
+            <form onSubmit={Login}>
+              <div className="form-group mt-2">
+                <input
+                  type="email"
+                  name="correo"
+                  className="form-control"
+                  /* autofocus inicia el cursor en este lugar del form */
+                  placeholder="Correo"
+                  required
+                  autoFocus
+                  onChange={(e) => setCorreo(e.target.value)}
+                  value={correo}
+                />
+              </div>
+              <div className="form-group mt-2">
+                <input
+                  type="password"
+                  name="contrasena"
+                  className="form-control"
+                  placeholder="contrasena"
+                  required
+                  onChange={(e) => setContrasena(e.target.value)}
+                  value={contrasena}
+                />
+              </div>
+              <div className="d-grid gap-2 m-2">
+                <button
+                  type="submit"
+                  className=" btn btn-success mt-3 btn-block"
+                >
+                  Aceptar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="col-lg-6 col-md-8 mx-auto">
-          <div className="card">
-            <div className="container text-center fa-4x">
-             
-              <i class="fas fa-user-lock"></i>
-            </div>
-            <div className="card-header text-center">Iniciar Sesion</div>
-  
-            <div className="card-boddy m-2">
-              <form onSubmit={Login}>
-                <div className="form-group mt-2">
-                  <input
-                    type="email"
-                    name="correo"
-                    className="form-control"
-                    /* autofocus inicia el cursor en este lugar del form */
-                    placeholder="Correo"
-                    required
-                    autoFocus
-                    onChange={(e) => setCorreo(e.target.value)}
-                    value={correo}
-                  />
-                </div>
-                <div className="form-group mt-2">
-                  <input
-                    type="password"
-                    name="contrasena"
-                    className="form-control"
-                    placeholder="contrasena"
-                    required
-                    onChange={(e) => setContrasena(e.target.value)}
-                    value={contrasena}
-                  />
-                </div>
-                <div className="d-grid gap-2 m-2">
-                  <button
-                    type="submit"
-                    className=" btn btn-success mt-3 btn-block"
-                  >
-                    Aceptar
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="text-primary text-center">
-            <Link to="/registrar">
-              {" "}
-              <h6>¿Eres Nuevo? Registrarme</h6>
-             
-              
-             
-            </Link>
-          </div>
-          <div className="text-primary text-center">
-            <Link to="/Recuperar_Contrasena/">
-              {" "}
-              
-              <h6>Olvide mi contraseña?</h6>
-              
-             
-            </Link>
-          </div>
+        <div className="text-primary text-center">
+          <Link to="/registrar">
+            {" "}
+            <h6>¿Eres Nuevo? Registrarme</h6>
+          </Link>
+        </div>
+        <div className="text-primary text-center">
+          <Link to="/Recuperar_Contrasena/">
+            {" "}
+            <h6>Olvide mi contraseña?</h6>
+          </Link>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
-export default Loging
+export default Loging;
--- src/components/nav.js
+++ src/components/nav.js
@@ -4,150 +4,176 @@ import { Link } from "react-router-dom";
 function Nav() {
   /* controlar menu admin o usuario */
   const [menu, setMenu] = useState(false);
+  const [admin, setAdmin] = useState(false);
   const id = sessionStorage.getItem("nombre");
   const id2 = sessionStorage.getItem("admin");
-  const token=  sessionStorage.getItem("token");
+  const token = sessionStorage.getItem("token");
+
   useEffect(() => {
     if (id === id2) {
-      setMenu(true);
+      setAdmin(true);
     }
   }, [id, id2]);
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      setMenu(true);
+    }
+  }, []);
 
   /* Salir dee sesion */
   const salir = () => {
     sessionStorage.clear();
+    setMenu(false);
   };
 
   return (
     <div>
-      { token?(<div>{menu ? (
-       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-       <div className="container-fluid">
-         <a className="navbar-brand" href="#">
-           ADMINISTRADOR
-         </a>
-         <button
-           className="navbar-toggler"
-           type="button"
-           data-bs-toggle="collapse"
-           data-bs-target="#navbarNavDropdown"
-           aria-controls="navbarNavDropdown"
-           aria-expanded="false"
-           aria-label="Toggle navigation"
-         >
-           <span className="navbar-toggler-icon"></span>
-         </button>
-         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-           <ul className="navbar-nav">
-             <li className="nav-item">
-               <a className="nav-link active" aria-current="page" href="#">
-                ADMNISTRAR PRODUCTOS
-               </a>
-             </li>
-             <li className="nav-item">
-               <a className="nav-link" href="#">
-                 ADMINISTRAR USUARIOS
-               </a>
-             </li>
-            
-             
-             <ul>
-     <li className="nav-item col-md-6 col-lg-3 col-xl-5">
-       <Link className="nav-link" to="/" onClick={() => salir()}>
-         Salir
-       </Link>
-     </li>
-   </ul>
-           </ul>
-         </div>
-       </div>
-     </nav>
-      ) : (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            USUARIO
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                 ADMNISTRAR PRODUCTOS
+      {menu ? (
+        <div>
+          {admin ? (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                  ADMINISTRADOR:{" "} {sessionStorage.getItem("nombre")}
                 </a>
-              </li>
-             
-             
-              
-              <ul>
-      <li className="nav-item col-md-6 col-lg-3 col-xl-5">
-        <Link className="nav-link" to="/" onClick={() => salir()}>
-          Salir
-        </Link>
-      </li>
-    </ul>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      )}</div>):(<div>
-         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              INGRESO
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                   LOGIN
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                   REGISTRO
-                  </a>
-                </li>
-               
-                
-                <ul>
-        <li className="nav-item col-md-6 col-lg-3 col-xl-5">
-          <Link className="nav-link" to="/" onClick={() => salir()}>
-            Salir
-          </Link>
-        </li>
-      </ul>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>)
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarNavDropdown"
+                  aria-controls="navbarNavDropdown"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                  className="collapse navbar-collapse"
+                  id="navbarNavDropdown"
+                >
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <a
+                        className="nav-link active"
+                        aria-current="page"
+                        href="#"
+                      >
+                        ADMNISTRAR PRODUCTOS
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        ADMINISTRAR USUARIOS
+                      </a>
+                    </li>
 
-      
-    }
-     
-      
+                    <ul>
+                      <li className="nav-item col-md-6 col-lg-3 col-xl-5">
+                        <Link
+                          className="nav-link"
+                          to="/"
+                          onClick={() => salir()}
+                        >
+                          Salir
+                        </Link>
+                      </li>
+                    </ul>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          ) : (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                  USUARIO:{" "} {sessionStorage.getItem("nombre")}
+                </a>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarNavDropdown"
+                  aria-controls="navbarNavDropdown"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                  className="collapse navbar-collapse"
+                  id="navbarNavDropdown"
+                >
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <a
+                        className="nav-link active"
+                        aria-current="page"
+                        href="#"
+                      >
+                        ADMNISTRAR PRODUCTOS
+                      </a>
+                    </li>
+
+                    <ul>
+                      <li className="nav-item col-md-6 col-lg-3 col-xl-5">
+                        <Link
+                          className="nav-link"
+                          to="/"
+                          onClick={() => salir()}
+                        >
+                          Salir
+                        </Link>
+                      </li>
+                    </ul>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          )}
+        </div>
+      ) : (
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">
+                INGRESO
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/" onClick={() => salir()}>
+                      LOGIN
+                    </Link>
+                  </li>
+
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to="/registrar"
+                      onClick={() => salir()}
+                    >
+                      REGISTRO
+                    </Link>
+                  </li>
+
+                 
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      )}
     </div>
   );
 }
